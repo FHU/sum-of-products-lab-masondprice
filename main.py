6eeff46483a8d178390bb696615917700db7fc9a
masondprice <mason.price@students.fhu.edu>
2024-01-25 16:36:23 -0600

    Update main.py
    reset with orig template

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
-
-
 def sum_of_products(list1, list2):
     sum = 0
     for i in range(len(list1)):
         sum += (int(list1[i]) * int(list2[i]))
     return sum
 
-
 if __name__ == '__main__':
+   #REMOVE PASS AND YOUR CODE GOES HERE
     list1 = input("Enter first number:").split()
     list2 = input("Enter second number:").split()
 
